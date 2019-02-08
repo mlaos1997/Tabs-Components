5970c1cc3794eecda9dc7d019ee5fd0726170b11
Marcelo Laos <mlaos1997@gmail.com>
2019-02-07 21:45:22 -0500

    update

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -16,10 +16,13 @@ class Dropdown {
     })
   }
 
-
+  toggleContent() {
+    
+    // Toggle the ".dropdown-hidden" class off and on
+    this.content;
+  }
+}
 
 
 // Nothing to do here, just study what the code is doing and move on to the Dropdown class
 let dropdowns = document.querySelectorAll('.dropdown').forEach( dropdown => new Dropdown(dropdown));
-
-let drop;
